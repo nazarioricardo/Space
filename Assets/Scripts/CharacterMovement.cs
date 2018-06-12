736ef18dab135753ea8dbc24bf7984c4396594dd
nazarioricardo <nazarioricardo@gmail.com>
2018-06-12 11:12:18 -0400

    Small Input Managing

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,7 +7,7 @@ public class CharacterMovement : MonoBehaviour {
 
 	public float speed = 5f;
 	public float turnSensitity = 5f;
-	public float jumpHeight = 10f;
+	public float jumpHeight = 1.5f;
 	public float dashDistance = 5f;
 
 	public Vector3 drag;
@@ -15,25 +15,30 @@ public class CharacterMovement : MonoBehaviour {
 	public LayerMask ground;
 	public Transform groundChecker;
 	public float mouseSensitivity = 100.0f;
-	private float clampAngle = 80.0f;
+	private float modAngle = 360.0f;
 
 	private float rotationY = 0.0f; // rotation around the up/y axis
 	private float rotationX = 0.0f; // rotation around the right/x axis
 	private CharacterController controller;
 	private Vector3 velocity;
-	private float gravity = 9.81f;
+	private float gravity = -9.81f;
 	private float groundDistance;
 	private bool isGrounded = false;
 
 	void Start () {
 		controller = GetComponent<CharacterController>();
-		groundDistance = transform.lossyScale.y / 2;
+        groundDistance = transform.lossyScale.y / 2;
+        groundChecker = transform.GetChild(0);
 		InitializeRotation ();
 	}
 	
 	void Update () {
 		MovePlayer ();
 		MouseLook ();
+        Jump();
+	}
+
+	void FixedUpdate () {
 	}
 
 	// Custom Methods
@@ -45,10 +50,11 @@ public class CharacterMovement : MonoBehaviour {
 		SetCursorToCenter ();
 	}
 
-	void MovePlayer () {
-		isGrounded = Physics.CheckSphere (groundChecker.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
-		if (isGrounded && velocity.y < 0)
-			velocity.y = 0f;
+    void MovePlayer () {
+        Debug.Log(controller.isGrounded);
+		//isGrounded = Physics.CheckSphere (groundChecker.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
+        //if (!isGrounded)
+            //Debug.Log("Not grounded");
 
 //		if (gameObject.transform.parent != null && isGrounded)
 //			Debug.Log ("Is in ship and grounded");
@@ -56,23 +62,31 @@ public class CharacterMovement : MonoBehaviour {
 		Vector3 move = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
 		controller.Move (controller.transform.TransformDirection (move * Time.deltaTime * speed));
 
-		if (Input.GetButtonDown ("Jump") && isGrounded) {
-			Debug.Log ("Jump Pressed");
-			velocity.y += Mathf.Sqrt (jumpHeight * gravity * Time.deltaTime * 2);
-		}
 
-		velocity.y -= gravity * Time.deltaTime;
 		controller.Move (controller.transform.TransformVector (velocity));
 	}
+
+    void Jump () {
+        if (controller.isGrounded && velocity.y < 0)
+            velocity.y = 0f;
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded) {
+            Debug.Log("Jump Pressed");
+            if (controller.isGrounded)
+                velocity.y += Mathf.Sqrt(jumpHeight * gravity * -0.05f);
+            //velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+    }
 
 	void MouseLook () {
 		float mouseX = Input.GetAxis("Right X");
 		float mouseY = -Input.GetAxis("Right Y");
-
-		rotationY += mouseX * mouseSensitivity * Time.deltaTime;
-		rotationX += mouseY * mouseSensitivity * Time.deltaTime * turnSensitity;
-
-		rotationX = Mathf.Clamp(rotationX, -clampAngle, clampAngle);
+        rotationX += mouseY * mouseSensitivity * Time.deltaTime;
+        rotationY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotationX = rotationX % modAngle;
+        rotationY = rotationY % modAngle;
 
 		Quaternion localRotation = Quaternion.Euler(0.0f, rotationY, 0.0f);
 		transform.localRotation = localRotation;
